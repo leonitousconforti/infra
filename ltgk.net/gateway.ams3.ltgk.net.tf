4d0114df4c0e83ea63a719dc1529f868cc6f2891
Leo Conforti <leoconforti@ltgk.net>
2025-06-03 15:57:39 +0000

    gateway droplets

--- ltgk.net/gateway.ams3.ltgk.net.tf
+++ ltgk.net/gateway.ams3.ltgk.net.tf
@@ -11,6 +11,31 @@ resource "digitalocean_droplet" "gateway_ams3" {
   tags          = ["gateway"]
   ssh_keys      = [data.digitalocean_ssh_key.personal.id]
   vpc_uuid      = data.digitalocean_vpc.ltgk-internal-ams3.id
+
+  connection {
+    timeout = "2m"
+    type    = "ssh"
+    user    = "root"
+    agent   = true
+    host    = self.ipv4_address
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "set -eu",
+      "export IFS=$'\n\t'",
+      "while pgrep -x apt > /dev/null; do sleep 1; done;",
+      "DEBIAN_FRONTEND=noninteractive apt-get update",
+      "DEBIAN_FRONTEND=noninteractive apt-get upgrade -y",
+      "DEBIAN_FRONTEND=noninteractive apt-get install -y wireguard",
+      "sysctl -w net.ipv4.ip_forward=1",
+      "echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf",
+      "iptables -t nat -A POSTROUTING -s ${data.digitalocean_vpc.ltgk-internal-ams3.ip_range} -o eth0 -j MASQUERADE",
+      "DEBIAN_FRONTEND=noninteractive apt-get install -y iptables-persistent",
+      "iptables-save > /etc/iptables/rules.v4",
+      "iptables-save > /etc/iptables/rules.v6",
+    ]
+  }
 }
 
 resource "digitalocean_project_resources" "ltgk_net_project_ams3_gateway" {
