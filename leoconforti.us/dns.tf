6700ce3c9c0a2bd6855addb84d8b8e24cf3faa8e
Leo Conforti <leoconforti@ltgk.net>
2025-06-03 14:35:37 +0000

    fix dns

--- leoconforti.us/dns.tf
+++ leoconforti.us/dns.tf
@@ -9,29 +9,29 @@ resource "digitalocean_project_resources" "leoconforti_us_project" {
   ]
 }
 
-resource "digitalocean_record" "ns1" {
-  domain = digitalocean_domain.leoconforti_us.name
-  type   = "NS"
-  name   = "leoconforti.us"
-  value  = "ns1.digitalocean.com."
-  ttl    = 1800
-}
+# resource "digitalocean_record" "ns1" {
+#   domain = digitalocean_domain.leoconforti_us.name
+#   type   = "NS"
+#   name   = "@"
+#   value  = "ns1.digitalocean.com."
+#   ttl    = 1800
+# }
 
-resource "digitalocean_record" "ns2" {
-  domain = digitalocean_domain.leoconforti_us.name
-  type   = "NS"
-  name   = "leoconforti.us"
-  value  = "ns2.digitalocean.com."
-  ttl    = 1800
-}
+# resource "digitalocean_record" "ns2" {
+#   domain = digitalocean_domain.leoconforti_us.name
+#   type   = "NS"
+#   name   = "@"
+#   value  = "ns2.digitalocean.com."
+#   ttl    = 1800
+# }
 
-resource "digitalocean_record" "ns3" {
-  domain = digitalocean_domain.leoconforti_us.name
-  type   = "NS"
-  name   = "leoconforti.us"
-  value  = "ns3.digitalocean.com."
-  ttl    = 1800
-}
+# resource "digitalocean_record" "ns3" {
+#   domain = digitalocean_domain.leoconforti_us.name
+#   type   = "NS"
+#   name   = "@"
+#   value  = "ns3.digitalocean.com."
+#   ttl    = 1800
+# }
 
 resource "digitalocean_record" "mx1" {
   domain   = digitalocean_domain.leoconforti_us.name
--- ltgk.page/dns.tf
+++ ltgk.page/dns.tf
@@ -9,26 +9,26 @@ resource "digitalocean_project_resources" "ltgk_page_project" {
   ]
 }
 
-resource "digitalocean_record" "ns1" {
-  domain = digitalocean_domain.ltgk_page.name
-  type   = "NS"
-  name   = "ltgk.page"
-  value  = "ns1.digitalocean.com."
-  ttl    = 1800
-}
+# resource "digitalocean_record" "ns1" {
+#   domain = digitalocean_domain.ltgk_page.name
+#   type   = "NS"
+#   name   = "@"
+#   value  = "ns1.digitalocean.com."
+#   ttl    = 1800
+# }
 
-resource "digitalocean_record" "ns2" {
-  domain = digitalocean_domain.ltgk_page.name
-  type   = "NS"
-  name   = "ltgk.page"
-  value  = "ns2.digitalocean.com."
-  ttl    = 1800
-}
+# resource "digitalocean_record" "ns2" {
+#   domain = digitalocean_domain.ltgk_page.name
+#   type   = "NS"
+#   name   = "@"
+#   value  = "ns2.digitalocean.com."
+#   ttl    = 1800
+# }
 
-resource "digitalocean_record" "ns3" {
-  domain = digitalocean_domain.ltgk_page.name
-  type   = "NS"
-  name   = "ltgk.page"
-  value  = "ns3.digitalocean.com."
-  ttl    = 1800
-}
+# resource "digitalocean_record" "ns3" {
+#   domain = digitalocean_domain.ltgk_page.name
+#   type   = "NS"
+#   name   = "@"
+#   value  = "ns3.digitalocean.com."
+#   ttl    = 1800
+# }
