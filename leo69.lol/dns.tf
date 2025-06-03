d84910fa8476e1cc6c82ba0323b3c2156f0f99de
Leo Conforti <leoconforti@ltgk.net>
2025-06-03 09:18:38 -0500

    test5

--- leo69.lol/dns.tf
+++ leo69.lol/dns.tf
@@ -5,7 +5,7 @@ resource "digitalocean_domain" "leo69_lol" {
 resource "digitalocean_project_resources" "leo69_lol_project" {
   project = data.digitalocean_project.project.id
   resources = [
-    digitalocean_domain.leo69_lol.id,
+    digitalocean_domain.leo69_lol.urn,
   ]
 }
 
@@ -13,7 +13,7 @@ resource "digitalocean_record" "ns1" {
   domain = digitalocean_domain.leo69_lol.name
   type   = "NS"
   name   = "leo69.lol"
-  value  = "ns1.digitalocean.com"
+  value  = "ns1.digitalocean.com."
   ttl    = 1800
 }
 
@@ -21,7 +21,7 @@ resource "digitalocean_record" "ns2" {
   domain = digitalocean_domain.leo69_lol.name
   type   = "NS"
   name   = "leo69.lol"
-  value  = "ns2.digitalocean.com"
+  value  = "ns2.digitalocean.com."
   ttl    = 1800
 }
 
@@ -29,6 +29,6 @@ resource "digitalocean_record" "ns3" {
   domain = digitalocean_domain.leo69_lol.name
   type   = "NS"
   name   = "leo69.lol"
-  value  = "ns3.digitalocean.com"
+  value  = "ns3.digitalocean.com."
   ttl    = 1800
 }
