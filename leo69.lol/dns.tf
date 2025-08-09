3be101bb7ada0a5522106196501cd5d1b027a2cd
Leo Conforti <leoconforti@ltgk.net>
2025-08-09 16:07:28 +0000

    ns dns records

--- leo69.lol/dns.tf
+++ leo69.lol/dns.tf
@@ -9,26 +9,26 @@ resource "digitalocean_project_resources" "leo69_lol_project" {
   ]
 }
 
-# resource "digitalocean_record" "ns1" {
-#   domain = digitalocean_domain.leo69_lol.name
-#   type   = "NS"
-#   name   = "@"
-#   value  = "ns1.digitalocean.com."
-#   ttl    = 1800
-# }
+resource "digitalocean_record" "ns1" {
+  domain = digitalocean_domain.leo69_lol.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns1.digitalocean.com."
+  ttl    = 1800
+}
 
-# resource "digitalocean_record" "ns2" {
-#   domain = digitalocean_domain.leo69_lol.name
-#   type   = "NS"
-#   name   = "@"
-#   value  = "ns2.digitalocean.com."
-#   ttl    = 1800
-# }
+resource "digitalocean_record" "ns2" {
+  domain = digitalocean_domain.leo69_lol.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns2.digitalocean.com."
+  ttl    = 1800
+}
 
-# resource "digitalocean_record" "ns3" {
-#   domain = digitalocean_domain.leo69_lol.name
-#   type   = "NS"
-#   name   = "@"
-#   value  = "ns3.digitalocean.com."
-#   ttl    = 1800
-# }
+resource "digitalocean_record" "ns3" {
+  domain = digitalocean_domain.leo69_lol.name
+  type   = "NS"
+  name   = "@"
+  value  = "ns3.digitalocean.com."
+  ttl    = 1800
+}
