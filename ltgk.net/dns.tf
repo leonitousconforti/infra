6ddd65c2c5131867662abd45d7219087355ba528
Leo Conforti <leoconforti@ltgk.net>
2025-08-09 19:40:11 +0000

    save

--- ltgk.net/dns.tf
+++ ltgk.net/dns.tf
@@ -1,16 +1,16 @@
-resource "digitalocean_domain" "ltgk_net" {
+data "digitalocean_domain" "ltgk_net" {
   name = "ltgk.net"
 }
 
 resource "digitalocean_project_resources" "ltgk_net_domain_project_assignment" {
   project = data.digitalocean_project.project.id
   resources = [
-    digitalocean_domain.ltgk_net.urn,
+    data.digitalocean_domain.ltgk_net.urn,
   ]
 }
 
 resource "digitalocean_record" "ns1" {
-  domain = digitalocean_domain.ltgk_net.name
+  domain = data.digitalocean_domain.ltgk_net.name
   type   = "NS"
   name   = "@"
   value  = "ns1.digitalocean.com."
@@ -18,7 +18,7 @@ resource "digitalocean_record" "ns1" {
 }
 
 resource "digitalocean_record" "ns2" {
-  domain = digitalocean_domain.ltgk_net.name
+  domain = data.digitalocean_domain.ltgk_net.name
   type   = "NS"
   name   = "@"
   value  = "ns2.digitalocean.com."
@@ -26,7 +26,7 @@ resource "digitalocean_record" "ns2" {
 }
 
 resource "digitalocean_record" "ns3" {
-  domain = digitalocean_domain.ltgk_net.name
+  domain = data.digitalocean_domain.ltgk_net.name
   type   = "NS"
   name   = "@"
   value  = "ns3.digitalocean.com."
@@ -34,7 +34,7 @@ resource "digitalocean_record" "ns3" {
 }
 
 resource "digitalocean_record" "mx1" {
-  domain   = digitalocean_domain.ltgk_net.name
+  domain   = data.digitalocean_domain.ltgk_net.name
   type     = "MX"
   name     = "@"
   value    = "fwd1.porkbun.com."
@@ -43,7 +43,7 @@ resource "digitalocean_record" "mx1" {
 }
 
 resource "digitalocean_record" "mx2" {
-  domain   = digitalocean_domain.ltgk_net.name
+  domain   = data.digitalocean_domain.ltgk_net.name
   type     = "MX"
   name     = "@"
   value    = "fwd2.porkbun.com."
@@ -52,7 +52,7 @@ resource "digitalocean_record" "mx2" {
 }
 
 resource "digitalocean_record" "dmarc" {
-  domain = digitalocean_domain.ltgk_net.name
+  domain = data.digitalocean_domain.ltgk_net.name
   type   = "TXT"
   name   = "_dmarc"
   value  = "v=DMARC1; p=quarantine; rua=mailto:25f8c5e6@mxtoolbox.dmarc-report.com; ruf=mailto:25f8c5e6@forensics.dmarc-report.com; fo=1"
@@ -60,7 +60,7 @@ resource "digitalocean_record" "dmarc" {
 }
 
 resource "digitalocean_record" "domainkey" {
-  domain = digitalocean_domain.ltgk_net.name
+  domain = data.digitalocean_domain.ltgk_net.name
   type   = "TXT"
   name   = "default._domainkey"
   value  = "v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC8158yxaFjZYq/gcbZ9pfsLcq7pkABLfl8q1AXWbAMpgZSUdpw1o/CKSHmVpnYL9pwLTYvLxpN9W8DkO5lSRFWxP/ljKCX/rJzSJ28MwI7UuPjbWpG43/zdIJc9yUP8F9ulmumnVinMEvQPVW49u1RPSDeCovTMWCrLXeRrRE86wIDAQAB"
@@ -68,7 +68,7 @@ resource "digitalocean_record" "domainkey" {
 }
 
 resource "digitalocean_record" "sfp" {
-  domain = digitalocean_domain.ltgk_net.name
+  domain = data.digitalocean_domain.ltgk_net.name
   type   = "TXT"
   name   = "@"
   value  = "v=spf1 include:_spf.porkbun.com ~all"
